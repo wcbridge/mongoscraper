e33f8736f4abe970edec300c4735d613668d364f
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-07-03 10:46:03 -0400

    deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/homeworkscraper"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/homeworkscraper";
 mongoose.connect(MONGODB_URI);
 
 
